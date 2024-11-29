7f0e9518977ef4104bd86f729efc5f858230f367
Arocha <alberto.jmas.rocha@gmail.com>
2024-11-29 00:33:37 +0000

    init

---
+++ main.py
@@ -0,0 +1,60 @@
+# With pip install manim
+# Para latex: no terminal do windows (nao funciona em bash):
+# winget install MikTeX.MikTeX
+# Correr no terminal
+# manim main.py [class] -qm
+
+
+
+from manim import *
+
+config.media_width = "75%"
+config.verbosity = "WARNING"
+
+#
+
+class CircleToSquare(Scene):
+    def construct(self):
+        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        green_square = Square(color=GREEN, fill_opacity=0.8)
+        self.play(Create(blue_circle))
+        self.wait()
+        
+        self.play(Transform(blue_circle, green_square))
+        self.wait()
+
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+
+
+class HelloLaTeX(Scene):
+    def construct(self):
+        tex = Tex(r"\LaTeX", font_size=144)
+        self.add(tex)
+
+
+class MathTeXDemo(Scene):
+    def construct(self):
+        rtarrow0 = MathTex(r"\xrightarrow{x^6y^8}", font_size=96)
+        rtarrow1 = Tex(r"$\xrightarrow{x^6y^8}$", font_size=96)
+
+        self.add(VGroup(rtarrow0, rtarrow1).arrange(DOWN))
+        
+
+class CorrectLaTeXSubstringColoring(Scene):
+    def construct(self):
+        equation = MathTex(
+            r"e^x = x^0 + x^1 + \frac{1}{2} x^2 + \frac{1}{6} x^3 + \cdots + \frac{1}{n!} x^n + \cdots",
+            substrings_to_isolate="x"
+        )
+        equation.set_color_by_tex("x", YELLOW)
+        self.add(equation)        
